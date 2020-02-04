961708871c015512921f4538ee6396727db2e8b7
Luc Patiny <luc@patiny.com>
2020-02-04 16:13:18 +0100

    feat: works in typed array

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -23,6 +23,28 @@ describe('Polynomial regression', () => {
     expect(result.toLaTeX(2)).toBe('f(x) = 0.18x^{2} - 0.19x + 0.85');
   });
 
+  it('degree 2 typed array', () => {
+    const x = new Float64Array([-3, 0, 2, 4]);
+    const y = new Float64Array([3, 1, 1, 3]);
+    const result = new PolynomialRegression(x, y, 2);
+
+    const expected = [0.850519, -0.192495, 0.178462];
+
+    for (let i = 0; i < expected.length; ++i) {
+      expect(result.coefficients[i]).toBeCloseTo(expected[i], 10e-6);
+      expect(result.powers[i]).toBe(i);
+    }
+
+    const score = result.score(x, y);
+    expect(score.r2).toBeGreaterThan(0.8);
+    expect(score.chi2).toBeLessThan(0.1);
+    expect(score.rmsd).toBeCloseTo(0.12);
+    expect(result.toString(4)).toBe(
+      'f(x) = 0.1785 * x^2 - 0.1925 * x + 0.8505',
+    );
+    expect(result.toLaTeX(2)).toBe('f(x) = 0.18x^{2} - 0.19x + 0.85');
+  });
+
   it('degree 5', () => {
     const x = [50, 50, 50, 70, 70, 70, 80, 80, 80, 90, 90, 90, 100, 100, 100];
     const y = [
